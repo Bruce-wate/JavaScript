1775857d162fbec526d7989a428e5b6275486522
Bruce wate <bruc3wate@gmail.com>
2025-01-14 14:19:12 +0200

    refatorar: melhorar a função de contagem e ajustar o layout do formulário

--- Modulo-E/exercicio1/comandos/index.js
+++ Modulo-E/exercicio1/comandos/index.js
@@ -1,34 +1,26 @@
 function contar(){
-    var inpt1 = window.document.querySelector("input#INP1")
-    var inpt2 = window.document.querySelector("input#INP2")
-    var passos = window.document.querySelector("input#step")
-    var count = window.document.querySelector("input#contador")
-    var res = window.document.querySelector("div#res")
-    var corpo = document.querySelector("main")
-    corpo.style.height = "300px";
-    
-    var n1 = Number(inpt1.value)
-    var n2 = Number(inpt2.value)
-    var p = Number (passos.value)
+    var INPT1 = document.querySelector("input#INPUT1")
+    var INPT2 = document.querySelector("input#INPUT2")
+    var step = document.querySelector("input#passo")
+    var ans = document.querySelector("div#res")
 
-    if (n1 == 0){
-        window.alert("Por favor, insira o valor do  INICIO")
-    } else if (n2 == 0){
-        window.alert("Por favor, insira o valor do FIM")
-    } else if (p == 0){
-        window.alert("Por favor, insira os passos entre os números")
+    var n1 = Number(INPT1.value)
+    var n2 = Number(INPT2.value)
+    var Pa = Number(step.value)
+
+    if (n1 == 0 || n2 == 0 || Pa == 0){
+        alert("Erro! Preencha todos os campos!")
     } else {
-        res.innerHTML = ("Contando... ")
-
-        for (c = n1; c <= n2; c += p)
-        res.innerHTML += c + ` \u{1F449} `   
-
-        for (c = n1; c >= n2; c -= p)
-        res.innerHTML += c + ` \u{1F449} `   
-    
-    } 
-
-    res.innerHTML +=  ` \u{1F3C1} ` 
-    
-    
+        ans.innerHTML = "Contando: <br>"
+        if (n1 < n2){
+            for (var c = n1; c <= n2; c += Pa){
+                ans.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for (var c = n1; c >= n2; c -= Pa){
+                ans.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        ans.innerHTML += `\u{1F3C1}`
     }
+}   
